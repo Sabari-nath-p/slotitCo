e395ac7ce7eace5c6484595b7d882b500377a723
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-11-18 18:28:17 +0530

    menucard

--- lib/HomeScreen/views/homeMenuCard.dart
+++ lib/HomeScreen/views/homeMenuCard.dart
@@ -13,8 +13,20 @@ class HomeMenuCard extends StatelessWidget {
       height: 145.h,
       width: 386.w,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          menucard(),
+              appText.primaryText(
+              text: 'Menu', fontWeight: FontWeight.w500, fontSize: 16.sp,color: const Color(0xFF232627)),
+              SpacerH(17.h),
+          Row(
+            children: [
+              _menucard('Manage','asset/images/manage.png'),
+              SpacerW(15.w),
+              _menucard('Settlement','asset/images/settlement.png'),
+              SpacerW(14.w),
+              _menucard('Profile','asset/images/profile.png'),
+            ],
+          ),
         ],
       ),
     );
@@ -23,10 +35,10 @@ class HomeMenuCard extends StatelessWidget {
 }
 
 
- menucard() {
+ _menucard(String text,image) {
     return Container(
           height: 106.h,
-          width: 119.h,
+          width: 119.w,
           decoration: BoxDecoration(
             color: const Color(0xFFFFFFFF),
             borderRadius: BorderRadius.circular(14)
@@ -35,20 +47,21 @@ class HomeMenuCard extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
+                alignment: Alignment.center,
                 height: 50.h,
                 width: 50.w,
                 decoration: BoxDecoration(
                   color: const Color(0xFFEBEDF1),
                   borderRadius: BorderRadius.circular(10)
                 ),
-                child: Image.asset('asset/images/manage.png',height: 30.h,width: 30.w,),
+                child: Image.asset(image,height: 30.h,width: 30.w,)
               ),
 
               SpacerH(14.h),
 
 
               appText.primaryText(
-              text: "Manage", fontWeight: FontWeight.w500, fontSize: 12.sp)
+              text: text, fontWeight: FontWeight.w500, fontSize: 12.sp,color: const Color(0xFF232627))
             ],
           ),
         );
