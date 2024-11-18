6cb7b211ee36853a9e794e74c73124ad5dfe5a4f
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-11-18 21:13:20 +0530

    home

--- lib/HomeScreen/views/appointmentListCard.dart
+++ lib/HomeScreen/views/appointmentListCard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sloti_co/main.dart';
 import 'package:sloti_co/src/appText.dart';
+import 'package:sloti_co/src/utils.dart';
 
 class Appointmentlistcard extends StatelessWidget {
   const Appointmentlistcard({super.key});
@@ -10,31 +11,42 @@ class Appointmentlistcard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 386.w,
+      padding: EdgeInsets.all(14),
       margin: EdgeInsets.only(bottom: 13.h),
       height: 158.h,
       decoration: BoxDecoration(
-        color: const Color(0xFFF3F4F4),
+        color: const Color(0xFFFFFFFF),
         borderRadius: BorderRadius.circular(14)
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
 
           appText.primaryText(
               text: 'Appointment Date', fontWeight: FontWeight.w500, fontSize: 12.sp,color: const Color(0xFF757D7F)),
+            
+          SpacerH(6.h),
 
 
           Row(
             children: [
               Image.asset('asset/images/clock.png',height: 14.h,width: 14.w,),
+              SpacerW(8.w),
               appText.primaryText(
                   text: 'Mon, July 21 - 09:00 - 10:45 AM', fontWeight: FontWeight.w400, fontSize: 12.sp,color: const Color(0xFF757D7F)),
             ],
           ),
+
+          SpacerH(13.5.h),
           
-          Image.asset('asset/images/line.png',height: 1.h,width: 358.w,),
+          Image.asset('asset/images/line.png',width: 358.w,),
+
+          SpacerH(27.5.h),
 
           appText.primaryText(
-              text: 'Sabarinath P', fontWeight: FontWeight.w500, fontSize: 14.sp,color: const Color(0x232627)),
+              text: 'Sabarinath P', fontWeight: FontWeight.w500, fontSize: 14.sp,color: const Color(0xFF232627)),
+
+          SpacerH(5.h),
           
           appText.primaryText(
               text: 'Hair Cut , Colouring , Facial', fontWeight: FontWeight.w400, fontSize: 12.sp,color: const Color(0xFF757D7F)),
