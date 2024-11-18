ab9edd35f5d394523e756c791232654ba7d71205
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-11-18 18:01:48 +0530

    edit

--- lib/HomeScreen/views/homeMenuCard.dart
+++ lib/HomeScreen/views/homeMenuCard.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sloti_co/HomeScreen/HomeScreenMain.dart';
+import 'package:sloti_co/src/appText.dart';
+import 'package:sloti_co/src/utils.dart';
 
 class HomeMenuCard extends StatelessWidget {
   const HomeMenuCard({super.key});
@@ -12,15 +14,42 @@ class HomeMenuCard extends StatelessWidget {
       width: 386.w,
       child: Column(
         children: [
-          Container(
-            height: 106.h,
-            width: 119.h,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10)
-            ),
-          )
+          menucard(),
         ],
       ),
     );
   }
+
 }
+
+
+ menucard() {
+    return Container(
+          height: 106.h,
+          width: 119.h,
+          decoration: BoxDecoration(
+            color: const Color(0xFFFFFFFF),
+            borderRadius: BorderRadius.circular(14)
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 50.h,
+                width: 50.w,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFEBEDF1),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Image.asset('asset/images/manage.png',height: 30.h,width: 30.w,),
+              ),
+
+              SpacerH(14.h),
+
+
+              appText.primaryText(
+              text: "Manage", fontWeight: FontWeight.w500, fontSize: 12.sp)
+            ],
+          ),
+        );
+  }
