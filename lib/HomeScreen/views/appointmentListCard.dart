13974963fb984a9f8e996e4d2e0a51d555248d7c
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-11-18 20:58:44 +0530

    edit

--- lib/HomeScreen/views/appointmentListCard.dart
+++ lib/HomeScreen/views/appointmentListCard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sloti_co/main.dart';
+import 'package:sloti_co/src/appText.dart';
 
 class Appointmentlistcard extends StatelessWidget {
   const Appointmentlistcard({super.key});
@@ -11,8 +12,34 @@ class Appointmentlistcard extends StatelessWidget {
       width: 386.w,
       margin: EdgeInsets.only(bottom: 13.h),
       height: 158.h,
+      decoration: BoxDecoration(
+        color: const Color(0xFFF3F4F4),
+        borderRadius: BorderRadius.circular(14)
+      ),
       child: Column(
-        children: [],
+        children: [
+
+          appText.primaryText(
+              text: 'Appointment Date', fontWeight: FontWeight.w500, fontSize: 12.sp,color: const Color(0xFF757D7F)),
+
+
+          Row(
+            children: [
+              Image.asset('asset/images/clock.png',height: 14.h,width: 14.w,),
+              appText.primaryText(
+                  text: 'Mon, July 21 - 09:00 - 10:45 AM', fontWeight: FontWeight.w400, fontSize: 12.sp,color: const Color(0xFF757D7F)),
+            ],
+          ),
+          
+          Image.asset('asset/images/line.png',height: 1.h,width: 358.w,),
+
+          appText.primaryText(
+              text: 'Sabarinath P', fontWeight: FontWeight.w500, fontSize: 14.sp,color: const Color(0x232627)),
+          
+          appText.primaryText(
+              text: 'Hair Cut , Colouring , Facial', fontWeight: FontWeight.w400, fontSize: 12.sp,color: const Color(0xFF757D7F)),
+
+        ],
       ),
     );
   }
