3cd168983b18ee4aede7d641b111370f6101e625
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-12-13 12:34:20 +0530

    add

--- lib/ServiceList/views/ServiceItemView.dart
+++ lib/ServiceList/views/ServiceItemView.dart
@@ -26,23 +26,60 @@ class ServiceItemView extends StatelessWidget {
           SpacerW(15.w),
 
           Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              appText.primaryText(
-                    text: 'Name of Service',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 16.sp,
-                    color: const Color(0xFF000000)),
+              Row(
+                children: [
+                  appText.primaryText(
+                        text: 'Name of Service',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16.sp,
+                        color: const Color(0xFF000000)),
+                  Image.asset('asset/images/rating.png',
+                    height: 28.h,
+                    width: 61.w,
+                )
+                ],
+              ),
+
+              SpacerH(6.h),
+
               appText.primaryText(
                     text: 'Loram leads the digital transformation of track maintenance.....',
                     fontWeight: FontWeight.w500,
                     fontSize: 12.sp,
-                    color: const Color(0xFF000000)),
-              appText.primaryText(
-                    text: 'Rs 6400',
-                    fontWeight: FontWeight.w600,
-                    fontSize: 17.sp,
-                    color: const Color(0xFF000000)),
+                    color: const Color(0xFF000000),
+                    maxLines: 1,
+                    ),
+
+              SpacerH(10.h),
+              
+              Row(
+                children: [
+                  appText.primaryText(
+                        text: 'Rs 6400',
+                        fontWeight: FontWeight.w600,
+                        fontSize: 17.sp,
+                        color: const Color(0xFF000000)),
+
+                        SpacerW(3.w),
+
+                  appText.primaryText(
+                        text: 'Rs 6400',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13.sp,
+                        color: const Color(0xFF000000)),
+
+                        SpacerW(60.w),
+                      
+                  appText.primaryText(
+                        text: '20 Mins',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13.sp,
+                        color: const Color(0xFF000000)),
+                ],
+              ),
               
             ],
           ),
